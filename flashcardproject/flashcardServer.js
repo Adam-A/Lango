6e8a9932a455b73a083e95c5a3171ea8f808acaf
cmnlee <cmnlee@ucdavis.edu>
2019-05-13 13:49:08 -0700

    added translation query functionality

--- flashcardproject/flashcardServer.js
+++ flashcardproject/flashcardServer.js
@@ -1,23 +1,77 @@
-const express = require('express')
-const port = 51490
+"use strict";
+
+const express = require('express');
+const http = require('http');
+const APIrequest = require('request');
+const APIkey = "AIzaSyD5oG-6TYOgYT4gq-y5vg-YcNy14mCtjuc";  // ADD API KEY HERE
+const url = "https://translation.googleapis.com/language/translate/v2?key=" + APIkey;
+const port = 51490;
+
+// An object containing the data expressing the query to the
+// translate API.
+// Below, gets stringified and put into the body of an HTTP PUT request.
+let requestObject = {
+    "source": "en",
+    "target": "ja",
+    "q": ["Hello!"]
+};
+
+console.log("English phrase: ", requestObject.q[0]);
 
 function queryHandler(req, res, next) {
     let url = req.url;
     let qObj = req.query;
     console.log(qObj);
     if (qObj.animal != undefined) {
-	res.json( {"beast" : qObj.animal} );
-	// res.send(qObj.animal)
+        res.json( {"beast" : qObj.animal} );
+        // res.send(qObj.animal)
     }
-    else if (qObj.word != undefined) {
-	var backwards = "";
-	for (var i = (qObj.word.length - 1); i >= 0; i--) {
-	    backwards += qObj.word.charAt(i);
-	}
-	res.json({"palindrome": qObj.word + backwards});
+    else if (qObj.english != undefined) {
+        requestObject.q = [qObj.english];
+        myFunc(res);
+        // res.json({"palindrome": qObj.word + backwards});
     }
     else {
-	next();
+        next();
+    }
+}
+
+function myFunc (res) {
+    APIrequest(
+        { // HTTP header stuff
+            url: url,
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            // will turn the given object into JSON
+            json: requestObject
+        },
+        // callback function for API request
+        APIcallback
+    );
+
+    function APIcallback (err, APIresHead, APIresBody) {
+        if ((err) || (APIresHead.statusCode != 200)) {
+            // API is not working
+            console.log("Got API error");
+            console.log(APIresBody);
+        }
+        else {
+            if (APIresHead.error) {
+                // API worked but is not giving you data
+                console.log(APIresHead.error);
+            }
+            else {
+                console.log("In Japanese: ",
+                    APIresBody.data.translations[0].translatedText);
+                console.log("\n\nJSON was:");
+                console.log(JSON.stringify(APIresBody, undefined, 2));
+                // print it out as a string, nicely formatted
+                res.json({
+                    "English" : requestObject.q[0], // Or wherever you have your english phrase contained
+                    "Japanese" : APIresBody.data.translations[0].translatedText
+                });
+            }
+        }
     }
 }
 
@@ -26,13 +80,12 @@ function fileNotFound(req, res) {
     res.type('text/plain');
     res.status(404);
     res.send('Cannot find '+url);
-    }
+}
 
 // put together the server pipeline
-const app = express()
-app.use(express.static('public'));  // can I find a static file? 
-app.get('/query', queryHandler );   // if not, is it a valid query?
+const app = express();
+app.use(express.static('public'));  // can I find a static file?
+app.get('/translate', queryHandler );   // if not, is it a valid query?
 app.use( fileNotFound );            // otherwise not found
 
-app.listen(port, function (){console.log('Listening...');} )
- 
+app.listen(port, function (){console.log('Listening on port ') + port;} );
---
+++ flashcardproject/public/flashcard.js
@@ -0,0 +1,40 @@
+function onButtonClick() {
+  let inputText = document.getElementById("word").value;
+  let url = "translate?english=" + inputText;
+  makeAjaxRequest(url);
+}
+
+function createAjaxRequest(method, url) {
+  let xhr = new XMLHttpRequest();
+  xhr.open(method, url, true);
+  return xhr;
+}
+
+function makeAjaxRequest(url) {
+  let xhr = createAjaxRequest('GET', url);
+  if (!xhr) {
+    alert('Ajax not supported');
+    return;
+  }
+
+  xhr.onload = function() {
+    // Get JSON string and turn into object.
+    let responseStr = xhr.responseText;
+    let object = JSON.parse(responseStr);
+
+    // Then call the function that displays
+    // the returned JSON text on the page.
+    displayTranslation(object.Japanese);
+  };
+
+  xhr.onerror = function() {
+    alert('Error: could not make the request.');
+  };
+
+  xhr.send();
+}
+
+function displayTranslation(translatedText) {
+  let outputText = document.getElementById("outputGoesHere");
+  outputText.textContent = "Translated Output: " + translatedText;
+}

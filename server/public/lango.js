44e644a53828419547286fe62834216bcf71fef1
Adam-A <adamalics@gmail.com>
2019-05-16 02:43:02 +0000

    added lang html and css

---
+++ server/public/lango.js
@@ -0,0 +1,151 @@
+'use strict';
+
+let sourceText = "";
+let targetText = "";
+
+
+/* const e = React.createElement;
+
+class LikeButton extends React.Component {
+    constructor(props) {
+	super(props);
+	this.state = { liked: false };
+    }
+
+    render() {
+	if (this.state.liked) {
+	    return 'You liked this.';
+	}
+
+	return e(
+	    'button',
+	    { onClick: () => this.setState({ liked: true }) },
+	    'Like'
+	);
+    }
+}
+*/
+
+//const domContainer = document.querySelector('#like_button_container');
+//ReactDOM.render(e(LikeButton), domContainer);
+
+// An element to go into the DOM
+
+var lango = React.createElement(
+    "h1",
+    { id: "logo" },
+    "Lango!"
+);
+
+// A component - function that returns some elements
+function FirstCard() {
+    return React.createElement(
+	"div",
+	{ className: "textCard" },
+	React.createElement(
+	    "p",
+	    null,
+	    "Hello, world!"
+	)
+    );
+}
+
+// Another component
+function FirstInputCard() {
+    return React.createElement(
+	"div",
+	{ className: "textCard" },
+	React.createElement("textarea", { onKeyPress: checkReturn })
+    );
+}
+
+// An element with some contents, including a variable
+// that has to be evaluated to get an element, and some
+// functions that have to be run to get elements.
+var main = React.createElement(
+    "main",
+    null,
+    lango,
+    React.createElement(FirstInputCard, null),
+    React.createElement(FirstCard, null)
+);
+
+ReactDOM.render(main, document.getElementById('root'));
+
+// onKeyPress function for the textarea element
+// When the charCode is 13, the user has hit the return key
+function checkReturn(event) {
+    console.log(event.charCode);
+}
+
+function onSubmitClick() {
+    let inputText = document.getElementById("phraseForm").value;
+    sourceText = inputText;
+    let url = "translate?source=" + inputText;
+    makeTranslationAjaxRequest(url);
+}
+
+function onSaveClick(){
+    if (sourceText != undefined && targetText != undefined) {
+	let url = `store?source=${sourceText}&target=${targetText}`;
+	makeStoreAjaxRequest(url);
+    }
+
+}
+function createAjaxRequest(method, url) {
+  let xhr = new XMLHttpRequest();
+  xhr.open(method, url, true);
+  return xhr;
+}
+
+function makeTranslationAjaxRequest(url) {
+  let xhr = createAjaxRequest('GET', url);
+  if (!xhr) {
+    alert('Ajax not supported');
+    return;
+  }
+
+    xhr.onload = function() {
+	//Get JSON string and turn into object.
+	let responseStr = xhr.responseText;
+	let object = JSON.parse(responseStr);
+	//Then call the function that displays
+	//the returned JSON text on the page.
+	displayTranslation(object.target);
+	targetText = object.target;
+  };
+
+  xhr.onerror = function() {
+    alert('Error: could not make the request.');
+  };
+
+  xhr.send();
+}
+
+function makeStoreAjaxRequest(url) {
+    let xhr = createAjaxRequest('GET', url);
+    if (!xhr) {
+	alert('Ajax not supported');
+	return;
+    }
+
+    xhr.onload = function() {
+	//Get JSON string and turn into object.
+	let responseStr = xhr.responseText;
+	let object = JSON.parse(responseStr);
+	//Then call the function that displays
+	//the returned JSON text on the page.
+	console.log(object);
+    };
+
+    xhr.onerror = function() {
+	alert('Error: could not make the request.');
+    };
+
+    xhr.send();
+}
+
+function displayTranslation(translatedText) {
+  let outputText = document.getElementById("outputGoesHere");
+  outputText.textContent = "Translated Output: " + translatedText;
+}

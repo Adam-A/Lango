96242ecb527c2781e5b617a3e8731a2f60dc7941
Adam Ali <adam@Adams-MacBook-Pro.local>
2019-06-01 20:14:19 -0700

    test

--- server/flashcardServer.js
+++ server/flashcardServer.js
@@ -247,7 +247,7 @@ app.get('/auth/redirect',
     // will come back here to send back the response
     // ...with a cookie in it for the Browser!
     function (req, res) {
-        console.log("LOG HERE " + req)
+        console.log("TEST " + res)
         console.log('Logged in and using cookies!')
         res.redirect('/user/lango.html');
     });

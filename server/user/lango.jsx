f6f78736aa2c64243ff1a5437ce15042c55be9e0
Adam Ali <adam@Adams-MacBook-Pro.local>
2019-06-06 01:50:37 -0700

    test

--- server/user/lango.jsx
+++ server/user/lango.jsx
@@ -229,10 +229,10 @@ class ReviewCardMain extends React.Component {
 
 
               <div class="scene scene--card">
-                <div class="card">
+                <ReviewCard class="card">
                     <div class="card__face card__face--front">front</div>
                 <div class="card__face card__face--back">back</div>
-                     </div>
+                     </ReviewCard>
                 </div>
 
               <InputCard>
